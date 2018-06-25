eaca679e14cc4d7e282a369f5cf389385a837bc1
e-qui-nox <yamuna.pixie@gmail.com>
2018-06-25 21:25:57 +0530

    First Commit

---
+++ Week 1/Cash.java
@@ -0,0 +1,39 @@
+﻿import java.util.Scanner;
+
+public class Cash {
+	public static void main(String []args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Change owed: ");
+	    double change = scan.nextDouble();
+	    
+	    while(change < 0) {
+	        System.out.println("Change owed: ");
+	        change = scan.nextDouble();
+	    }
+
+	    int cents = (int)Math.round(change * 100);
+	    
+	    int coins = 0;
+	    if(cents >= 25)
+	    {
+	        coins += cents / 25;
+	        cents %= 25;
+	    }
+	    if(cents >= 10)
+	    {
+	        coins += cents / 10;
+	        cents %= 10;
+	    }
+	    if(cents >= 5)
+	    {
+	        coins += cents / 5;
+	        cents %= 5;
+	    }
+	    if(cents >= 1)
+	    {
+	        coins += cents;
+	    }
+
+	    System.out.println(coins);
+	}
+}
---
+++ Week 1/Mario.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+
+public class Mario {
+	public static void main(String []args) {
+		int height;
+		Scanner scan = new Scanner(System.in);
+	    do
+	    {
+	        System.out.println("height: ");
+	        height = scan.nextInt();
+	    }
+	    while(height < 0  || height > 23);
+	    
+	    for(int i = height; i >= 1; i--) {
+	        for(int j = 0; j <= height; j++){
+	            if(j >= (i - 1))
+	            	System.out.print("#");
+	            else
+	            	System.out.print(" ");
+	        }
+	        System.out.print("\n");
+	    }
+	}
+}
